c8bd55f87acdad9484981b308177578a14848c6d
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-12-29 20:48:57 -0800

    version1.0

---
+++ proyecto10.5/proyecto10.5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto10._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Cantidad de valores que vamos a analizar: ");
+                int cantidad = int.Parse(Console.ReadLine());
+                int mayorOigual100 = 0;
+                int numero;
+
+                for (int i = 0; i < cantidad; i++)
+                {
+                    Console.Write("Escribe el numero: ");
+                    numero = int.Parse(Console.ReadLine());
+
+                    if (numero >= 1000)
+                    {
+                        mayorOigual100 = mayorOigual100 + 1;
+                    }
+                }
+                Console.WriteLine("Hay "+mayorOigual100+" numeros mayor o igual a 1000.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha introducido un dato invalido.");                
+            }
+            Console.ReadKey();
+        }
+    }
+}
